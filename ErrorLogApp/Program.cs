e8f176d3bcaa302be3be432021362c3a381efe80
aganapathy <aganapathy@AHC-LHY3PF72A>
2017-08-16 14:02:48 -0500

    Add project files.

---
+++ ErrorLogApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exceptionless;
+using System.Configuration;
+
+namespace ErrorLogApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiKey;
+            apiKey = ConfigurationManager.AppSettings["apiKey"];
+            ExceptionlessClient.Default.Startup(apiKey);
+            int x=100;
+            int y=0;
+            int z;
+            z = x / y;
+            Console.WriteLine("Result:" + z);
+
+        }
+    }
+}
