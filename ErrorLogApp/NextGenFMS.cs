cc403af7622ba3cb68ed8db42bdc34cec4257925
aganapathy <aganapathy@AHC-LHY3PF72A>
2017-08-16 15:39:33 -0500

    Showcase Exception logging and Custom Exception Logging.

---
+++ ErrorLogApp/NextGenFMS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorLogApp
+{
+    public class NextGenFMS
+    {
+        public string Client;
+        public string User;
+        public string Role;
+        public string Module;
+        public string FormName;
+
+    }
+}
--- ErrorLogApp/Program.cs
+++ ErrorLogApp/Program.cs
@@ -10,17 +10,45 @@ namespace ErrorLogApp
 {
     class Program
     {
+        public static NextGenFMS objNextGenFMS;
         static void Main(string[] args)
         {
+
             string apiKey;
             apiKey = ConfigurationManager.AppSettings["apiKey"];
             ExceptionlessClient.Default.Startup(apiKey);
-            int x=100;
-            int y=0;
-            int z;
-            z = x / y;
+            
+            int x = 100;
+            int y = 0;
+            int z=0;
+            //z = x / y;
             Console.WriteLine("Result:" + z);
 
+            objNextGenFMS = new NextGenFMS();
+            objNextGenFMS.Client = "ALL Family of Companies";
+            objNextGenFMS.User = "John Doe";
+            objNextGenFMS.Module = "Scheduling";
+            objNextGenFMS.FormName = "Create a Job Schedule";
+            objNextGenFMS.Role = "Manager";
+            CustomErrorLogging(objNextGenFMS);
+
+        }
+
+        public static void CustomErrorLogging(NextGenFMS objEx)
+        {
+
+            try
+            {
+                int x = 100;
+                int y = 0;
+                int z;
+                z = x / y;
+            }
+
+            catch (Exception ex)
+            {
+                ex.ToExceptionless().MarkAsCritical().AddObject(objEx).Submit();
+            }
         }
     }
-}
+    }
